ad2381ac105caf95404cf8b4163137a3e12f4ca9
mano92fuentesjimenez <m92fuentes@gmail.com>
2020-08-08 12:24:16 -0400

    add redux-devtools support

--- src/client/store.ts
+++ src/client/store.ts
@@ -1,4 +1,4 @@
-import { Action, applyMiddleware, createStore as _createStore, Store as ReduxStore } from 'redux'
+import { Action, applyMiddleware, createStore as _createStore, Store as ReduxStore, compose } from 'redux'
 import { ThunkAction, ThunkDispatch } from 'redux-thunk'
 import { create } from './middlewares'
 import reducers from './reducers'
@@ -7,9 +7,19 @@ export const middlewares = create(
   window.localStorage && window.localStorage.log,
 )
 
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose
+  }
+}
+const composeEnhancers = typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : compose
 export const createStore = () => _createStore(
   reducers,
-  applyMiddleware(...middlewares),
+  composeEnhancers(applyMiddleware(...middlewares)),
 )
 
 export default createStore()
