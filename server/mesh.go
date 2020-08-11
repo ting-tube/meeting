8eecd405e2975f1e67209408f9ea25db23ccaeb9
Viktor Panichkin <viktor.panichkin@rwth-aachen.de>
2020-08-11 12:59:16 +0200

    https instead of http

--- server/mesh.go
+++ server/mesh.go
@@ -61,7 +61,7 @@ func NewMeshHandler(loggerFactory LoggerFactory, wss *WSS) http.Handler {
 					}
 
           http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-					resp, err := http.Post("http://localhost:8882/room", "application/json", bytes.NewBuffer(bytesRepresentation))
+					resp, err := http.Post("https://localhost:8882/room", "application/json", bytes.NewBuffer(bytesRepresentation))
 					if err != nil {
 						log.Printf("Error sending request to kurento server: %v", err)
 					}
